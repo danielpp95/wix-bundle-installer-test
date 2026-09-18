cf62f5bdcba8ce29107cdce743f2e6e2d304eeaf
Daniel <danielpp95@gmail.com>
2026-09-18 21:15:29 +0100

    Add project files.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ MauiApp1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1;
+
+using Foundation;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
